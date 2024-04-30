4610f81be229d122cdebd16de4c3c90e66f2fec5
MateusLVolkers <mateuslvolkers@gmail.com>
2024-04-29 21:47:20 -0300

    Alguns estudos

---
+++ Fundamentos/src/collections/Lista.java
@@ -0,0 +1,11 @@
+package collections;
+
+public class Lista {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
--- Fundamentos/src/collections/SetHashSetConjuntos.java
+++ Fundamentos/src/collections/SetHashSetConjuntos.java
@@ -1,11 +1,38 @@
 package collections;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class SetHashSetConjuntos {
     public static void main(String[] args) {
 
+        /* HashSet não obedece a ordem de inserção.
+        */
+        //Set<String> nomes = new HashSet<>();
+        Set<String> nomes = new TreeSet<>();
+        nomes.add("Mateus");
+        nomes.add("José");
+        nomes.add("Mandy");
+        nomes.add("Pedro");
 
+        Set<String> nomesComuns = new HashSet<>();
+        nomesComuns.add("João");
+        nomesComuns.add("José");
 
+        System.out.println(nomes.contains("José"));
+        System.out.println(nomes.remove("José"));
+        System.out.println(nomes.size());
 
+        //União de conjuntos
+        nomes.addAll(nomesComuns);
+        System.out.println(nomes.size());
+        for(String nome: nomes) {
+            System.out.println(nome);
+        }
 
+        //Interseção de conjuntos
+        nomes.retainAll(nomesComuns);
+        System.out.println(nomes);
     }
 }
