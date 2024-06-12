a5bfbcec887f1ee6b0fabbd50a679794e4057926
MateusLVolkers <mateuslvolkers@gmail.com>
2024-06-11 21:53:44 -0300

    Iniciando a lógica do campo minado

---
+++ cm/src/model/temp.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Iterator;
+
+public class temp {
+	
+	public static void main (String[] args) {
+		System.out.println("Teste");
+		int manasnasdasd = 2;
+		
+		System.out.println(manasnasdasd);
+	}
+}
